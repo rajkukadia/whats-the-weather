9edf06760e56a39b469047275d347cc0c89a7762
Raj Kukadia <rkukadia@intralinks.com>
2018-08-18 19:58:30 -0400

    design ready

--- app/src/main/java/com/example/rkukadia/whatstheweather/MainActivity.java
+++ app/src/main/java/com/example/rkukadia/whatstheweather/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.rkukadia.whatstheweather;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+    public void getWeather(View view){
+
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+    }
+
+
 }
